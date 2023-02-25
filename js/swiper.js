535bac08e2f2ddda7ebe803676bfb31b62139f94
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-02-26 02:25:30 +0500

    Добавил свайпер

---
+++ js/swiper.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	loop: true,
+	
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  
+  });
